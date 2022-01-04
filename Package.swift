fb83e4837d8c435f47c43b0e19159945084fff7c
jean torres <j.torres@mycoachsport.com>
2022-01-04 15:51:29 +0100

    copy ressources

--- Package.swift
+++ Package.swift
@@ -24,7 +24,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FlagPhoneNumber",
-            dependencies: ["libPhoneNumber"])
+            dependencies: ["libPhoneNumber"],
+            resources: [.copy("Sources/FlagPhoneNumber/Resources/countryCodes.json")])
     ]
 )
 
